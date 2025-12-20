5e0345952d7f761e227103584ccfe69fc819a1b1
Demias Syihab <120578896+maskprivate1457@users.noreply.github.com>
2025-12-21 01:03:24 +0700

    Update Profile.tsx

--- client/pages/Profile.tsx
+++ client/pages/Profile.tsx
@@ -418,7 +418,7 @@ export default function Profile() {
               <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 text-sm">
                 <div>
                   <p className="flex items-center pl-2 gap-2 text-foreground/70">
-                    <img src="https://cdn-icons-png.flaticon.com/512/6522/6522516.png" className="w-3.5 h-3.5 filter hue-rotate-180 contrast-125" alt="NIS" />
+                    <img src="https://cdn-icons-png.flaticon.com/512/6522/6522516.png" className="w-3.5 h-3.5 mr-2 filter hue-rotate-180 contrast-125" alt="NIS" />
                     NIS
                   </p>
                   <p className="font-semibold text-foreground pl-4">{profile.nis}</p>
